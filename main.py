66f71ff97e0b863ae28b6fb5133a09d4daf956ec
Jhonnatan Gomes <jhonnatangomesdeoliveira@gmail.com>
2026-08-31 20:25:08 -0300

    adicionei mais duas linhas no códigp

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 filmes = []
 
-filme = input('Digite o nome de um filme:')
+filme = input('Digite o nome de um filme: ')
 filmes.append(filme)
 
-filme = input('Digite outro nome de filme:')
+filme = input('Digite outro nome de filme: ')
+filmes.append(filme)
+
+filme = input("Digite um último filme: ")
 filmes.append(filme)
 
 print(filmes)
